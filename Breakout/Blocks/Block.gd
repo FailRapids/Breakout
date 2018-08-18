c3c5099742071af2f61f6637065e60d82795d252
Travis Davis <travisdavis_p@outlook.com>
2018-08-17 19:04:18 -0500

    Added hp to blocks

--- Breakout/Blocks/Block.gd
+++ Breakout/Blocks/Block.gd
@@ -1,5 +1,9 @@
 extends Area2D
-
+export(int) var hp = 1
 func _on_Block_body_entered(body):
-	if body.is_in_group("Ball"):
+	
+	if body.is_in_group("Ball") and hp == 0:
 		self.queue_free()
+	elif body.is_in_group("Ball"):
+		hp -= 1
+		
