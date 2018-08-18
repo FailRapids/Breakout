7682068f03f25001956731d2ee879925bfdc00fd
Travis Davis <travisdavis_p@outlook.com>
2018-08-17 19:05:03 -0500

    Tweaks

--- Breakout/Paddles/States/Idle.gd
+++ Breakout/Paddles/States/Idle.gd
@@ -5,6 +5,7 @@ var Max_Speed
 
 func enter(prev_state):
 	.enter(prev_state)
+	self.owner.velocity = Vector2()
 	self.Acceleration = $"../Move".Acceleration 
 	self.Max_Speed = $"../Move".Max_Speed
 	
--- Breakout/Paddles/States/Move.gd
+++ Breakout/Paddles/States/Move.gd
@@ -9,7 +9,7 @@ func enter(prev_state):
 func exit():
 	.exit()
 	self.owner.velocity.x = 0
-
+	self.owner.velocity.y = 0
 func update(dt):
 	.update(dt)
 	
@@ -19,7 +19,7 @@ func update(dt):
 func physics_update(dt): 
 	var direction = owner.move_direction
 	if direction.x != 0:
-		owner.velocity = owner.move_and_slide(
+		owner.move_and_collide(
 			owner.apply(dt, Vector2(self.Acceleration,0),Vector2(direction.x, 0),
 			
 			Vector2(
@@ -27,15 +27,7 @@ func physics_update(dt):
 				owner.velocity.y)
 		))
 	else:
-		owner.velocity = owner.move_and_slide(
-			owner.apply(dt, Vector2(6.5*self.Acceleration,0),Vector2(0, 0),
-			
-			Vector2(
-				clamp(owner.velocity.x,-self.Max_Speed,self.Max_Speed),
-				owner.velocity.y)
-		))
-		if (abs(owner.velocity.x) <= 10):
-			return "Idle" 
+		return "Idle"
 	return ""
 
 	
